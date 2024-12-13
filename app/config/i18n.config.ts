399bbde4b3c94971b0fb5a101cf71b6584f9aa0f
abdallah-shaltout <abdallahshaltout2002@gmail.com>
2024-12-13 14:23:31 +0200

    fix default lang:ar

--- app/config/i18n.config.ts
+++ app/config/i18n.config.ts
@@ -2,7 +2,7 @@ import ar from '~/locales/ar.json'
 import en from '~/locales/en.json'
 export default defineI18nConfig(() => ({
     fallbackLocale: 'ar',
-    
+    locale: 'ar',
     messages: {
         en: en,
         ar: ar,
